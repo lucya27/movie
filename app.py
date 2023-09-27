b0ca193e727e2a1d16d0181a17487191928c1099
lucya27 <ciakirana828@gmail.com>
2023-09-27 22:07:20 +0700

    update commit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-from bs4 import BeautifulSoup
-
-from flask import Flask, render_template, request, jsonify
-from pymongo import MongoClient
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -27,7 +29,7 @@ def movie_post():
     comment_receive = request.form['comment_give']
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = request.get(url_receive,headers=headers)
-    soup = BeautifulSoup(data.text, 'html.parser')
+    soup =  BeautifulSoup(data.text, 'html.parser')
     og_image = soup.select_one('meta[property="og:image"]')
     og_title = soup.select_one('meta[property="og:title"]')
     og_description = soup.select_one('meta[property="og:description"]')
